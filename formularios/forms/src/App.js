94f969c4208f825f719019ac02cbcf13c1af6270
santiago-mnd <santi_ga1020@hotmail.com>
2021-10-17 15:31:03 -0500

    Módulo formularios | Terminado

--- formularios/forms/src/App.js
+++ formularios/forms/src/App.js
@@ -1,22 +1,78 @@
+import React, { useState } from 'react';
 
-function App() {
-  const submit = (e) => {
-    e.preventDefault()
-    const data = Array.from(new FormData(e.target))
-    console.log(Object.fromEntries(data));
-  }
-  return (
-    <form onSubmit={submit}>
-      <div>
-        <span>
-          lala
-        </span>
-        <input name='campo' />
-      </div>
-      <input name='campo2'/>
-      <input type='submit' value='Enviar' />
-    </form>
-  );
+const App = () => {
+    const [value, setValue] = useState({ 
+        normal:'', 
+        texto:'', 
+        select:'', 
+        check: false, 
+        estado: 'feliz' })
+    const handleChange = ({ target }) => {
+        console.log(target.type, target.checked);
+        console.log(target.name);
+        setValue((state) => ({
+            ...state,
+            [target.name]: target.type === 'checkbox'
+                ? target.checked
+                : target.value
+        }))
+    }
+    console.log(value);
+    return (
+        <div>
+            {value.normal.length < 5 ? <span>Longitud mínima de 5</span> : null}
+            <input name='normal' type='text' value={value.normal} onChange={handleChange} />
+            <textarea name='texto' onChange={handleChange} value={value.texto} />
+
+            <select value={value.select} name='select' onChange={handleChange}>
+                <option value=''>-- Sleccione --</option>
+                <option value='chanchofeliz'>Chancho feliz</option>
+                <option value='chanchitofeli'>Chanchito feli</option>
+                <option value='chanchitotriste'>Chanchito triste</option>
+                <option value='Feli'>Feli</option>
+            </select>
+
+
+            {/* Input tipo Checkbox
+            Este tipo de input no rescibirá una propiedad de tipo value, sino una llamado checked.
+             */}
+            <input
+            type='checkbox'
+            name='check'
+            onChange={handleChange}
+            checked={value.check}
+            />
+
+             <div>
+                 <label>Chancho: </label>
+                 <input 
+                 onChange={handleChange} 
+                 type='radio' 
+                 value='feliz' 
+                 name='estado'
+                 checked={value.estado === 'feliz' ? true : false}
+                 /> 
+                 Feliz
+                 <input
+                 onChange={handleChange} 
+                 type='radio' 
+                 value='triste' 
+                 name='estado'
+                 checked={value.estado === 'triste' ? true : false}
+                 /> 
+                 Triste
+                 <input 
+                 onChange={handleChange} 
+                 type='radio' 
+                 value='feli' 
+                 name='estado' 
+                 checked={value.estado === 'feli' ? true : false}
+                 /> 
+                 Feli
+             </div>
+
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ formularios/forms/src/uncontrolled-react-way.js
@@ -0,0 +1,26 @@
+import React, { useRef } from 'react';
+
+const App = () => {
+    const input = useRef()
+    const file = useRef()
+    const submit = () => {
+        console.log(input.current.value)
+        console.log(file.current.files[0])
+        const form = new FormData()
+        form.append('archivo', file.current.files[0])
+        form.append('campo', input.current.value)
+        fetch('/lala', { method: 'POST', body: form })
+    }
+    return (
+        <div>
+            <div>
+                <span>Lala</span>
+                <input type='text' name='campo' ref={input} />
+                <input type='file' ref={file}/>
+            </div>
+            <input type='submit' value='Enviar' onClick={submit} />
+        </div>
+    )
+}
+
+export default App
